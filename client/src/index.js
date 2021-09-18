af1bd2ae42b6db73e5aaf8401a7b0d7374ec905f
soli <solimantwali@gmail.com>
2021-09-18 13:14:18 -0400

    trying new architecture. need to connect cockroachdb to backend

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import { React, useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  
+    <App />
+  ,
+  document.getElementById('root')
+);
+
+
